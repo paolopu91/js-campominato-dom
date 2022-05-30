b13618a1e44a57d8f24bfa6979117b1b4c56111b
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-30 20:23:35 +0200

    aggiunte modifiche bg color div

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
-
-
-
-
-
-
-//Creo un adEventListener prima dell'inizio del gioco per permettere al giocatore di premere su start the game
-const start = document.getElementById("start")
-start.addEventListener("click", function()  {
-
-
 //Variabile globale, per richiamare il mio elemento HTML utilizabile in tutto il mio foglio js
 const gridContainer = document.querySelector(".grid-container");
 
-//creo la mia funzione per generare le bombe dentro al mio gioco
+// imposto la variabile per la condizione di gameover;
+let gameover = 0;
 
- 
+// funzione per la creazione delle bombe nel mio gioco 
 function bombList (maxBombsNumber){
     //qui dentro ora creo un array vuoto in cui andranno i miei numeri che ho generato per le bombe
     const contenitoreBombe = [];
@@ -27,57 +17,75 @@ function bombList (maxBombsNumber){
             contenitoreBombe.push(randomNumbers);
         }
     }
+    // richiamo la mia funzione per farla funzionare
     return contenitoreBombe;
-}
-    
-    // quanto voglio che sia grande la griglia? inteso in numero di quadrati che può contenere
+} 
+// funzione per il numero di celle che deve essere presente nel mio gioco
 function cellGrid (orizontalCells , verticalCells){
-    
+
+    // variabile del numero delle mie celle
     const cellsNumber = orizontalCells * verticalCells;
+
+    // variabile delle mie bombe
     const contenitoreBombe = bombList(cellsNumber)
+
     console.log(contenitoreBombe);
     console.log(cellsNumber);
     
     //questa formula va messa fuori il nostro ciclo for perchè sennò ad ogni ciclo si ripete
+    //questo fa riferimenti al ciclo for sottostante!!
     gridContainer.style.width = `calc(100px * ${orizontalCells})`
-    
-
     // creo il mio ciclo per far si che si vedano le mie celle
     for (let i = 1 ; i<=cellsNumber; i++){
         // creo una variabile per i miei numeri crescenti
-
         const celle = document.createElement("div");
         celle.classList.add("celle");
         celle.innerHTML = `<span>${i}</span>`;
         //append per aggiungere un elemento virtuale ad un elemento che abbiamo creato
         gridContainer.append(celle);
 
-        // creo l'elemento per far cambiare colore alle celle al click
+function eventoClick(cellGrid,bombList){
+// creo l'elemento per far cambiare colore alle celle al click
         celle.addEventListener("click", function () {
-            this.classList.add('bgAzzurro');
-        })
-        
-        celle.toggleAttribute("click" , function(){
-        this.classList.add('bg');
-        })
+            // questa qui è la mia costante per vedere il numero dell'indice delle mie caselle
+            const celleIndex = +this.dataset.indice
 
+            //creo delle condizioni per fermare il gioco quando si avverano
+            if(this.classList.contains('bg-red') || this.classList.contains('bgAzzurro') || gameover){
+                return
+            }
+            celle.classList.add("bgAzzurro");
+            celle.classList.remove("bg-red");
+            console.log("Hai selezionato ", +this.dataset.indice)
+            
+            if(contenitoreBombe.includes(cellsNumber)){
+                celle.classList.add("bg-red")
+                gameover = +true;
+                alert("Hai perso!")
+            }
+            
+        })
     }
-
+    eventoClick(cellGrid(), bombList())
 }
-const play = document.getElementById('play')
-
-alert("Seleziona , prima di iniziare, il livello di difficoltà")
-
-play.addEventListener("click", function(){
-    gridContainer.classList.remove("Easy", "Medium", "Hard")
-    gridContainer.innerHTML="";
-if (document.getElementById('Easy').selected) {
-    cellGrid(10, 10)
-} else if (document.getElementById('Medium').selected) {
-    cellGrid(9, 9)
-} else if (document.getElementById('Hard').selected) {
-    cellGrid(7, 7)
 }
-})
+        
+//Creo il mio evento on-click alla fine, dopo aver creato le mie funzioni, ovviamente voglio che certi eventi accadano all'interno del mio click!
 
-})
+//creo la mia costante play(fa riferimento al mio html) per far cambiare numero delle mie celle in base al livello di difficoltà.
+//la richiamo fuori dal mio add eventListner perchè deve esistere a prescindere del click dell'utente.
+    const play = document.getElementById('play')
+
+    play.addEventListener("click", function(){
+
+        gridContainer.classList.remove("Easy", "Medium", "Hard")
+        gridContainer.innerHTML="";
+
+    if (document.getElementById('Easy').selected) {
+        cellGrid(10, 10)
+    } else if (document.getElementById('Medium').selected) {
+        cellGrid(9, 9)
+    } else if (document.getElementById('Hard').selected) {
+        cellGrid(7, 7)
+    }
+    })
