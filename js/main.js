17e14b7612d3d484a4478816deff638153318248
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-30 16:33:37 +0200

    aggiunte modifiche

--- js/main.js
+++ js/main.js
@@ -13,23 +13,31 @@ start.addEventListener("click", function()  {
 const gridContainer = document.querySelector(".grid-container");
 
 //creo la mia funzione per generare le bombe dentro al mio gioco
-function bombNumberList (cellGrid, ){
+function bombList (cellGrid, maxBombNumber = 16){
     //qui dentro ora creo un array vuoto in cui andranno i miei numeri che ho generato per le bombe
-    const containerBomb = [];
-    console.log(containerBomb);
+    const contenitoreBombe = [];
+    console.log("contenitoreBombe")
     //creo il mio ciclo per generare 16 numeri random da andare ad inserire nel mio array
-    do{
+    for(let i = 0 ; i < contenitoreBombe.length; i++){
         // qui inserirò la mia operazione matematica per generare numeri random casuali
-        const randomNumber = Math.round(Math.random() * cellGrid) + 1;
-        console.log(randomNumber);
+        let randomNumbers = Math.floor(Math.random() * cellGrid);
+        console.log("randomNumbers");
         //creo la mia condizione per far si che dentro al mio array ci siano tutti numeri diversi.
-        if(!containerBomb.includes(randomNumber)){
-            containerBomb.push(randomNumber);
+        if(!contenitoreBombe.includes(randomNumbers)){
+            contenitoreBombe.push(randomNumbers);
         }
-    }while(containerBomb);
-
-    return containerBomb;
+    }
+    return contenitoreBombe;
 }
+bombList(16)
+    
+    
+    
+    
+        
+       
+        
+     
 
 
     // quanto voglio che sia grande la griglia? inteso in numero di quadrati che può contenere
@@ -67,6 +75,8 @@ function cellGrid (orizontalCells , verticalCells){
 }
 const play = document.getElementById('play')
 
+alert("Seleziona , prima di iniziare, il livello di difficoltà")
+
 play.addEventListener("click", function(){
     gridContainer.classList.remove("Easy", "Medium", "Hard")
     gridContainer.innerHTML="";
@@ -79,7 +89,4 @@ if (document.getElementById('Easy').selected) {
 }
 })
 
-cellGrid(10, 10);
-cellGrid(8, 8);
-cellGrid(7, 7);
 })
